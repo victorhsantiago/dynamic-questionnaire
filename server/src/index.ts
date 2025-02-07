20056057235ed28abbede0b3b7ea3c04ac5c4b9b
Victor Santiago <victorhsantiago3@gmail.com>
2025-02-07 16:10:10 +0100

    feat: add cors middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Express } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes";
 
@@ -8,6 +9,7 @@ const app: Express = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 app.listen(port, () => {
